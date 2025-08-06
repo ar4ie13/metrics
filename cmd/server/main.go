e38a7748fe2f96332a6a7e825bb4e4d4c948239f
ar4ie13 <ar4ie13@icloud.com>
2025-08-06 15:33:45 +0300

    autotests updated

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"net/http"
+)
 
+type gauge float64
+type counter int64
+
+func main() {
+	err := http.ListenAndServe("0.0.0.0:8080", http.FileServer(http.Dir(".")))
+	if err != nil {
+		panic(err)
+	}
 }
